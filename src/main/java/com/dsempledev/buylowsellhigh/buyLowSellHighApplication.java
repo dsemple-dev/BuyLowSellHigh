5d95a4ab7efee59fd1d4f666fa244e5183578f67
sempled <doug.semple@computershare.co.uk>
2022-05-29 17:26:27 +0100

    logging started

--- src/main/java/com/dsempledev/buylowsellhigh/buyLowSellHighApplication.java
+++ src/main/java/com/dsempledev/buylowsellhigh/buyLowSellHighApplication.java
@@ -1,5 +1,7 @@
 package com.dsempledev.buylowsellhigh;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +15,7 @@ public class buyLowSellHighApplication {
 
     private final String FILE1 = "src/main/resources/ChallengeSampleDataSet1.txt";
     private final String FILE2 = "src/main/resources/ChallengeSampleDataSet2.txt";
+    private static final Logger logger = LogManager.getLogger(buyLowSellHighApplication.class);
 
     public static void main(String[] args)
     {
@@ -32,23 +35,32 @@ public class buyLowSellHighApplication {
 
             pricesFileHandler fReader = new pricesFileHandler();
 
+            //logging test
+            logger.debug("Debugging log");
+            logger.info("Info log");
+            logger.warn("Hey, This is a warning!");
+            logger.error("Oops! We have an Error. OK");
+            logger.fatal("Damn! Fatal error. Please fix me.");
+
             //TO DO
-            //add logging using log4j2
-            //seperate logging by debug, error for prod
+            //add logging
+            //set logging to info for prod
+            //add debug log messages
+            //configure logging to a file
             //pull repeated sections for each set below into a method
             //consider and annotate SOLID "this is the S..."
 
-            System.out.println("SET 1");
+            logger.info("SET 1");
             ArrayList<Float> thePrices = fReader.readWithStreamTokenizer(FILE1);
             buyLowSellHighProcessor set1 = new buyLowSellHighProcessor(thePrices);
             //set1.matriceTest();
-            System.out.println(set1.getTheBestTrade());
+            logger.info(set1.getTheBestTrade());
 
-            System.out.println("SET 2");
+            logger.info("SET 2");
             thePrices = fReader.readWithStreamTokenizer(FILE2);
             buyLowSellHighProcessor set2 = new buyLowSellHighProcessor(thePrices);
             //set2.matriceTest();
-            System.out.println(set2.getTheBestTrade());
+            logger.info(set2.getTheBestTrade());
         };
     }
 }
